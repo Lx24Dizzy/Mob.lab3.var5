7b56c32f615968d6613674dc65f1c4c7e9026ec9
Тюлеубаев Радмир <104508501+Lx24Dizzy@users.noreply.github.com>
2022-10-11 16:56:12 +0600

    Add files via upload

---
+++ 2/TSN_DEMO_03_Questionnaire/src/main/java/kz/talipovsn/questionnaire/MainActivity2.java
@@ -0,0 +1,26 @@
+package kz.talipovsn.questionnaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    //Кнопка переход
+    public void onExit(View v) {
+        finishAffinity();
+    }
+    public void back(View v){
+        Intent intent = new Intent(MainActivity2.this, ResultActivity.class);
+        startActivity(intent);
+
+    }
+
+}
